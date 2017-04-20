6911267ef8e158dd8870b49318cea12181d087cd
Fane <fane247@gmail.com>
2017-04-20 17:11:30 +0100

    added views and TicTacToe Controller

---
+++ src/main/java/org/FaneFonseka/TicTacToe/GUI/TicTacToeMainController.java
@@ -0,0 +1,20 @@
+package org.FaneFonseka.TicTacToe.GUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import javax.security.auth.login.AppConfigurationEntry;
+
+/**
+ * Created by Fane on 20/04/2017.
+ */
+public class TicTacToeMainController extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+
+
+}
